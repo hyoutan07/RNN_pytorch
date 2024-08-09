0e7d8fc088a1dd34c100c4b10d7825784c338546
nakano taiki <nakanotaiki@kobayashinoMacBook.local>
2024-08-09 11:49:09 +0900

    update LSTM

--- LSTM.py
+++ LSTM.py
@@ -12,11 +12,24 @@ class ptb:
     def load_data(split):
         return np.random.randint(0, 100, 1000), {}, {}
 
+def device():
+    """環境毎に利用できるアクセラレータを返す"""
+    if torch.backends.mps.is_available():
+        # macOS w/ Apple Silicon or AMD GPU
+        return "mps"
+    if torch.cuda.is_available():
+        # NVIDIA GPU
+        return "cuda"
+    return "cpu"
+
+DEVICE = device()
+print(f"device: {DEVICE}")
+
 # Hyper parameter
 wordvec_size = 100
 hidden_size = 100
 batch_size = 10
-T = 5
+T = 20
 learning_rate = 0.01
 epochs = 100
 
@@ -53,7 +66,7 @@ class TimeEmbedding(nn.Module):
 class TimeAffine(nn.Module):
     def __init__(self, hidden_size, vocab_size, T):
         super(TimeAffine, self).__init__()
-        self.affine = nn.Linear(hidden_size, vocab_size, bias=False)
+        self.affine = nn.Linear(hidden_size, vocab_size)
         self.T = T
 
     def forward(self, h_results):
@@ -66,15 +79,28 @@ class TimeAffine(nn.Module):
 class LSTMCell(nn.Module):
     def __init__(self, input_size, hidden_size): # 初期化
         super(LSTMCell, self).__init__()
-        self.linear_x = nn.Linear(input_size, hidden_size, bias=False)
-        self.linear_h = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.linear_x = nn.Linear(input_size, 4*hidden_size, bias=False)
+        self.linear_h = nn.Linear(hidden_size, 4*hidden_size, bias=False)
+        self.bias = nn.Parameter(torch.randn(4*hidden_size))
 
-    def forward(self, x, h): # 順伝播
-        # print(x.shape)
+    def forward(self, x, h, c): # 順伝播
         x = self.linear_x(x)
         h = self.linear_h(h)
-        h_next = torch.tanh(x + h)
-        return h_next
+        A = torch.tanh(x + h + self.bias)
+
+        f = A[:, :hidden_size]
+        g = A[:, hidden_size: 2*hidden_size]
+        i = A[:, 2*hidden_size: 3*hidden_size]
+        o = A[:, 3*hidden_size:]
+
+        f = F.sigmoid(f)
+        g = F.tanh(g)
+        i = F.sigmoid(i)
+        o = F.sigmoid(o)
+        
+        c_next = f * c + g * i
+        h_next = o * F.tanh(c)
+        return h_next, c_next
 
 # TimeLSTM definition
 class TimeLSTM(nn.Module):
@@ -87,40 +113,51 @@ class TimeLSTM(nn.Module):
         self.layer = LSTMCell(input_size, hidden_size)
         self.stateful = stateful
     
-    def forward(self, X, hs=None):
-        if not self.stateful or hs is None:
+    def forward(self, X, hs=None,cs=None):
+        if not self.stateful or hs is None or cs is None:
             h = torch.zeros((self.batch_size, self.hidden_size))
+            c = torch.zeros((self.batch_size, self.hidden_size))
         else:
             h = hs
+            c = cs
 
         h_results = torch.empty((self.batch_size, self.T, self.hidden_size))
-        print("X size: ", X.size(0))
         for t in range(self.T):
-            # print(h)
-            # print(t)
-            # print(h.shape)
-            # print(h_results)
             h_results[:, t, :] = h
-            h = self.layer(X[:, t, :], h)
+            h, c = self.layer(X[:, t, :], h, c)
 
         return h_results
 
 class simpleLSTM(nn.Module):
-    def __init__(self, vocab_size, hidden_size, wordvec_size, batch_size, T):
+    def __init__(self, vocab_size, hidden_size, wordvec_size, batch_size, T, dropout=True):
         super(simpleLSTM, self).__init__()
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
         self.wordvec_size = wordvec_size
         self.batch_size = batch_size
         self.T = T
+        self.dropout = dropout
         
         self.time_embedding = TimeEmbedding(self.vocab_size, self.wordvec_size, self.T)
-        self.time_rnn = TimeLSTM(self.wordvec_size, self.hidden_size, self.batch_size, self.T)
+        self.time_LSTM_1 = TimeLSTM(self.wordvec_size, self.hidden_size, self.batch_size, self.T)
+        self.time_LSTM_2 = TimeLSTM(self.hidden_size, self.hidden_size, self.batch_size, self.T)
         self.time_affine = TimeAffine(self.hidden_size, self.vocab_size, self.T)
+        
+        if self.dropout:
+            self.dropout_1 = nn.Dropout()
+            self.dropout_2 = nn.Dropout()
+            self.dropout_3 = nn.Dropout()
         
     def forward(self, idx, hs=None):
         X = self.time_embedding(idx)
-        H = self.time_rnn(X, hs)
+        if self.dropout:
+            X = self.dropout_1(X)
+        H = self.time_LSTM_1(X, hs)
+        if self.dropout:
+            H = self.dropout_2(H)
+        H = self.time_LSTM_2(X, hs)
+        if self.dropout:
+            H = self.dropout_3(H)
         V = self.time_affine(H)
         output = F.softmax(V)
         return output
@@ -157,7 +194,7 @@ dataset = PTBDataset(xs, ts, T)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
 # Create model
-model = simpleLSTM(vocab_size, hidden_size, wordvec_size, batch_size, T)
+model = simpleLSTM(vocab_size, hidden_size, wordvec_size, batch_size, T).to(DEVICE)
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 losses = []
 
@@ -167,6 +204,8 @@ for epoch in range(epochs):
     cnt = 0
 
     for x, label in dataloader:
+        x = x.to(DEVICE)
+        label = label.to(DEVICE)
         # テンソルに変換
         ts_tensor = torch.tensor(label, dtype=torch.long)
         # ワンホットエンコーディング
--- test.py
+++ test.py
@@ -1,10 +1,5 @@
 import torch
+print(torch.__version__)  # PyTorchのバージョンを確認
+print(torch.backends.mps.is_available())  # MPSが利用可能か確認
+print(torch.backends.mps.is_built())  # MPSがPyTorchにビルドされているか確認
 
-# サイズ [10, 100] のテンソルを作成
-tensor1 = torch.randn(10, 5, 100)
-
-# サイズ [4, 50] のテンソルを作成
-tensor2 = torch.randn(10, 100)
-
-# 代入によるエラーの発生
-tensor1[:, 4, :] = tensor2
